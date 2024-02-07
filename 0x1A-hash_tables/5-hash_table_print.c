4bb3bd1d703f800dac5c184f5c9b8f67ced04bad
Golhun <geraldduah1@gmail.com>
2024-02-07 10:43:34 +0000

    a function that prints a hash table.

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ * they appear in the array of the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    hash_node_t *node;
+    unsigned long int i;
+    unsigned char comma_flag = 0;
+
+    /* Check if the hash table is invalid */
+    if (ht == NULL)
+        return;
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        /* Check if the current index is not empty */
+        if (ht->array[i] != NULL)
+        {
+            /* Print comma separator if necessary */
+            if (comma_flag == 1)
+                printf(", ");
+
+            /* Traverse the linked list at the current index */
+            node = ht->array[i];
+            while (node != NULL)
+            {
+                /* Print key-value pair */
+                printf("'%s': '%s'", node->key, node->value);
+
+                /* Move to the next node */
+                node = node->next;
+
+                /* Print comma separator if there are more nodes */
+                if (node != NULL)
+                    printf(", ");
+            }
+
+            /* Set comma flag to indicate that a comma has been printed */
+            comma_flag = 1;
+        }
+    }
+    printf("}\n");
+}
