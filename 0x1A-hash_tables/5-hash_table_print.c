325514a8aa652046d17de9e09dcd6ff0b1a7bb45
Golhun <geraldduah1@gmail.com>
2024-02-08 05:29:32 +0000

    Betty correction for 5-hash_table_print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,42 +9,42 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *node;
-    unsigned long int i;
-    unsigned char comma_flag = 0;
+hash_node_t *node;
+unsigned long int i;
+unsigned char comma_flag = 0;
 
-    /* Check if the hash table is invalid */
-    if (ht == NULL)
-        return;
+/* Check if the hash table is invalid */
+if (ht == NULL)
+return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        /* Check if the current index is not empty */
-        if (ht->array[i] != NULL)
-        {
-            /* Print comma separator if necessary */
-            if (comma_flag == 1)
-                printf(", ");
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+/* Check if the current index is not empty */
+if (ht->array[i] != NULL)
+{
+/* Print comma separator if necessary */
+if (comma_flag == 1)
+printf(", ");
 
-            /* Traverse the linked list at the current index */
-            node = ht->array[i];
-            while (node != NULL)
-            {
-                /* Print key-value pair */
-                printf("'%s': '%s'", node->key, node->value);
+/* Traverse the linked list at the current index */
+node = ht->array[i];
+while (node != NULL)
+{
+/* Print key-value pair */
+printf("'%s': '%s'", node->key, node->value);
 
-                /* Move to the next node */
-                node = node->next;
+/* Move to the next node */
+node = node->next;
 
-                /* Print comma separator if there are more nodes */
-                if (node != NULL)
-                    printf(", ");
-            }
+/* Print comma separator if there are more nodes */
+if (node != NULL)
+printf(", ");
+}
 
-            /* Set comma flag to indicate that a comma has been printed */
-            comma_flag = 1;
-        }
-    }
-    printf("}\n");
+/* Set comma flag to indicate that a comma has been printed */
+comma_flag = 1;
+}
+}
+printf("}\n");
 }
